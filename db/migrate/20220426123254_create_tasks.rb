babcecdd33ec86cc61349e4acb8f566328eddfab
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-30 16:43:57 +0900

    ログイン・ログアウト機能追加

--- db/migrate/20220426123254_create_tasks.rb
+++ db/migrate/20220426123254_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     create_table :tasks do |t|
       t.string :name
       t.text :description
+      t.integer :user_id
 
       t.timestamps
     end
